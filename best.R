07b8f211f5fa6afcf8099065fe6ccc93f167b3b4
DerekBryant <dwbryan@nppd.com>
2017-12-06 13:14:54 -0600

    20171206 1313 DWB added a validation step for the outcome. Also found how to call the columns with periods in them for SQL.

--- best.R
+++ best.R
@@ -2,12 +2,13 @@
 best <- function(state, outcome){
   
   ## 20171204 1634 - DWB - test variables 
-  ## state = "NE"
-  ## outcome = "heart failure"
+  ## state = "BB"
+  ##outcome = "heart failure"
   
   ## load prefered librarys
   ## 20171204 1634 - DWB - may not need to call sqldf?
   ## library()
+  
   
   ## Read outcome data
   readOutcomesFile <- read.csv("U:/Coursera/Assignments/2_R_Programming/week_4/Assignment3_DATA/outcome-of-care-measures.csv",
@@ -20,16 +21,34 @@ best <- function(state, outcome){
   ## get the valid states in the data
   valid_states <- sqldf::sqldf("Select State FROM readOutcomesFile GROUP BY State")
   
-  ## get the valid outcomes
-  ## TODO
+  ##20171206 DWB - following is test SQL to figure out issue with mortality rate
+  ##query
+  ##20171206 1309 DWB - below is valid just need the [] in the mortality rate query
+  test_qry_1 <- sqldf::sqldf("Select [Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack] as 'HA_Mortality_Rate'
+                              FROM readOutcomesFile")
   
   ## Check to see if state is valid
-  if((state %in% valid_states$State)=FALSE){
-    print("invalid state")
+  if(!(state %in% valid_states$State)){
+    stop("invalid state")
   }
-  
+
   ##Check to see if outcome is valid
-  ## TODO
+  if(outcome == "heart attack"){
+    #20171206 1310 below error out on the name of columns with '.'
+    #To make this valid surround in []
+    # best_Mortality <- sqldf::sqldf("Select State,
+    #                                Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack as 'HA_Mortality_rate' 
+    #                                FROM readOutcomesFile 
+    #                                ORDER BY Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack DESC")
+  }
+  else if(outcome == "heart failure"){
+    print("we have a heart failure")
+  }
+  else if(outcome == "pneumonia"){
+    print("we have pneumonia")
+  }
+  else 
+    stop("invalid outcome")
   
   
   ## Return hospital name in that state with the lowest 30-day death
